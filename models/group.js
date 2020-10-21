6a68178b11040fa943675315623911987855598d
rizkyakhid <rizkyakhid@gmail.com>
2020-10-21 19:00:21 +0700

    mvc-base

---
+++ models/group.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Group.belongsToMany(models.User, {
+        through: models.UserGroup,
+        foreignKey: "GroupId"
+      })
+      Group.hasMany(models.UserGroup)
+    }
+  };
+  Group.init({
+    group_name: DataTypes.STRING,
+    post: DataTypes.STRING,
+    comment: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Group',
+  });
+  return Group;
+};
---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasMany(models.Post)
+      User.belongsToMany(models.Group, {
+        through: models.UserGroup,
+        foreignKey: "UserId"
+      })
+      User.hasMany(models.UserGroup)
+    }
+  };
+  User.init({
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    profile_pict: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
