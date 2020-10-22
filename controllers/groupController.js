82c52b2a072da180128ea9b58ce18ff2c9610d96
rizkyakhid <rizkyakhid@gmail.com>
2020-10-22 18:22:16 +0700

    tambahan

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,6 +1,17 @@
 const { Group } = require('../models/index')
 
 class GroupController {
+  static showGroup(req, res) {
+    Group.findAll({order: [["id", "ASC"]]})
+    .then(groupData => {
+      console.log(groupData)
+      res.render('./group/groupList.ejs', {groupData})
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
   static addForm(req, res) {
     res.render('./group/addGroup.ejs')
   }
@@ -9,10 +20,9 @@ class GroupController {
     let obj = {
       group_name: req.body.group_name
     }
-    console.log(obj)
     Group.create(obj)
     .then(() => {
-      res.redirect('/')
+      res.redirect('/group')
     })
     .catch(err => {
       res.send(err)
@@ -20,11 +30,58 @@ class GroupController {
   }
 
   static editForm(req, res) {
-
+    let id = +req.params.id
+    Group.findByPk(id)
+    .then(groupData => {
+      res.render('./group/editGroup.ejs', {groupData})
+    })
+    .catch(err => {
+      res.send(err)
+    })
   }
 
   static edit(req, res) {
-    
+    let id = +req.params.id
+    let obj = {
+      group_name: req.body.group_name
+    }
+    Group.update(obj, {
+      where: {
+        id: id
+      },
+    })
+    .then(() => {
+      res.redirect('/group')
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static delete(req, res) {
+    let id = +req.params.id
+    Group.destroy({
+      where: {
+        id: id
+      }
+    })
+    .then(() => {
+      res.redirect('/group')
+    })
+    .catch(err => {
+      res.send(err)
+    })
+  }
+
+  static groupPage(req, res) {
+    let id = +req.params.id
+    Group.findByPk(id)
+    .then(groupData => {
+      res.render('./group/group.ejs', {groupData})
+    })
+    .catch(err => {
+      res.send(err)
+    })
   }
 }
 
--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,10 +1,12 @@
 const GroupController = require('../controllers/groupController')
 const router = require('express').Router()
 
+router.get('/', GroupController.showGroup)
 router.get('/add', GroupController.addForm)
 router.post('/add', GroupController.add)
-// router.get('/editGroupName/:id', (req, res) => {res.send('This is edit group name!')})
-// router.post('/editGroupName/:id')
-// router.get('/delete/:id', (req, res) => {res.send('This is delete group!')})
+router.get('/:id', GroupController.groupPage)
+router.get('/edit/:id', GroupController.editForm)
+router.post('/edit/:id', GroupController.edit)
+router.get('/delete/:id', GroupController.delete)
 
 module.exports = router
